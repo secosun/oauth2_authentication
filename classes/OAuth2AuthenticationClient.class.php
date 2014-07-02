581acca81522c974db3a20aec337133cae8441bb
Colan Schwartz <colan@58704.no-reply.drupal.org>
2014-07-02 14:04:12 -0400

    Moved all of the functionality into a class so that it may be overridden.

---
+++ classes/OAuth2AuthenticationClient.class.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * @file
+ * Base class for OAuth2 Authentication clients.
+ */
+
+/**
+ * Description of OAuth2AuthenticationClient class.
+ *
+ * The OAuth2AuthenticationClient class is used to authenticate users based on
+ * valid accesss tokens provided by an OAuth2 server.  Once users are logged in,
+ * their sessions will contain the tokens necessary for gaining access to remote
+ * resources.  Valid remote users who do not exist locally will be created.
+ *
+ * Feel free to subclass in order to override anything done here.  If you do,
+ * make sure to add the new class to the configuration.
+ */
+class OAuth2AuthenticationClient {
+
+  /**
+   * The username of the user whose access is being requested.
+   */
+  protected $username = NULL;
+
+  /**
+   * The password of the user whose access is being requested.
+   */
+  protected $password = NULL;
+
+  /**
+   * Construct an OAuth2\OAuth2AuthenticationClient object.
+   *
+   * @param string $username
+   *   The username of the user whose authentication is requested.
+   * @param string $password
+   *   The username of the user whose authentication is requested.
+   */
+  public function __construct($username, $password) {
+    // Set the username and password for later use.
+    $this->username = $username;
+    $this->password = $password;
+  }
+
+  /**
+   * Determines if a user with a provided name and password exists remotely.
+   *
+   * @return
+   *   TRUE if the user exists remotely; FALSE otherwise.
+   *
+   * @todo Return TRUE if a valid token is returned.
+   */
+  public function userExistsRemotely() {
+
+    // Configure the OAuth2 client.
+    $oauth2_config = array(
+      'auth_flow'      => 'user-password',
+      'token_endpoint' => variable_get('oauth2_authentication_token_endpoint', ''),
+      'client_id'      => variable_get('oauth2_authentication_client_id', ''),
+      'client_secret'  => variable_get('oauth2_authentication_client_secret', ''),
+      'scope'          => variable_get('oauth2_authentication_scope', ''),
+      'username'       => $this->username,
+      'password'       => $this->password,
+    );
+
+    // Attempt to get an access token.
+    try {
+      $oauth2_client = new OAuth2\Client($oauth2_config);
+      $access_token = $oauth2_client->getAccessToken();
+    }
+    catch (Exception $e) {
+      drupal_set_message($e->getMessage(), 'error');
+    }
+
+    // Code goes here.
+
+    return FALSE;
+  }
+
+  /**
+   * Create a new user based on the successful validation of a remote user.
+   *
+   * This function creates a new local Drupal user if a corresponding remote
+   * user exists, but doesn't exist here yet.
+   *
+   * @return
+   *   A fully-loaded $user object upon successful creation or FALSE on failure.
+   */
+  public function createUserLocally() {
+
+    // Get the user's e-mail address from some remote service.
+    $email = $this->getUserEmailAddress();
+
+    // Create a list of user information.
+    $user = array(
+      'name'   => $this->username,
+      'pass'   => $this->password,
+      'mail'   => $email,
+      'status' => 1,
+      'init'   => $email,
+      'roles'  => array(
+        DRUPAL_AUTHENTICATED_RID => 'authenticated user',
+      ),
+    );
+
+    // Save the new user.
+    return user_save(NULL, $user);
+  }
+
+  /**
+   * Fetches the e-mail address of the user to be created.
+   *
+   * @return
+   *   The user's e-mail address.
+   *
+   * @todo Add a new configuration option for the class name to instantiate.  By
+   *   default, it will be OAuth2AuthenticationClient.
+   */
+  protected function getUserEmailAddress() {
+    return '';
+  }
+
+}
+
